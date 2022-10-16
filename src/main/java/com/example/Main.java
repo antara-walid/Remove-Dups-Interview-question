84eb45a454fe10c869b85a3fb936a68a63887705
antara-walid <Antara.walid.27@gmail.com>
2022-10-16 11:57:34 +0100

    remove duplicates first virsion with O(n2) complexity

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,15 +1,44 @@
 package com.example;
 
+import org.w3c.dom.Node;
+
 import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
+        ListNode node3 = new ListNode(7, null);
+        ListNode node2 = new ListNode(4, node3);
+        ListNode node1 = new ListNode(4, node2);
+        ListNode head = new ListNode(5, node1);
+
+
+        //
+
     }
 
 
-    public void removeDublicates()
-    {
+    public static void removeDublicates(ListNode head) {
+        ListNode current = head;
+        ListNode cursor = head.next;
 
+        while (current.next != null) {
+            // first case
+            if(current.val == current.next.val){
+                current.next = current.next.next;
+            }
+
+            while (cursor.next != null) {
+                if (current.val == cursor.next.val) {
+                    // remove duplicate
+                    cursor.next = cursor.next.next;
+
+                } else {
+                    cursor = cursor.next;
+                }
+            }
+        }
     }
 }
