0f0a0c2e1d92a7f2a0fe395b37e0e1e2b36fbe9d
antara-walid <Antara.walid.27@gmail.com>
2022-10-16 12:24:36 +0100

    first version removeDuplicates2 with O(n) time complexity

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,8 @@
 package com.example;
 
 
+import java.util.HashSet;
+
 public class Main {
 
 
@@ -8,14 +10,15 @@ public class Main {
         ListNode node3 = new ListNode(7, null);
         ListNode node2 = new ListNode(4, node3);
         ListNode node1 = new ListNode(4, node2);
-        ListNode head = new ListNode(4, node1);
+        ListNode head = new ListNode(5, node1);
 
 
         //before
         print(head);
 
         //test
-        removeDuplicates(head);
+        //removeDuplicates(head);
+        removeDuplicates2(head);
 
         //after
         System.out.println("*******");
@@ -47,6 +50,24 @@ public class Main {
                 current = current.next;
             }
 
+        }
+    }
+
+    public static void removeDuplicates2(ListNode head) {
+        ListNode current = head;
+        HashSet<Integer> set = new HashSet<>();
+        set.add(head.val);
+
+        while (current.next != null) {
+            if(set.contains(current.next.val))
+            {
+                //delete node
+                current.next = current.next.next;
+            }
+            else{
+                set.add(current.next.val);
+                current = current.next;
+            }
         }
     }
 
