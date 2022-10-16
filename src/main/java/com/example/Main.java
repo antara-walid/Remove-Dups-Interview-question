4738cba0c5389b883d828edc0fa3cdc9bc357b1f
antara-walid <Antara.walid.27@gmail.com>
2022-10-16 11:33:18 +0100

    first commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+    }
+
+
+    public void removeDublicates()
+    {
+
+    }
+}
